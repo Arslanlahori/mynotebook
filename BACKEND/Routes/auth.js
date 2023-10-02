38d0b5a1318799334eb82ed1dd3a1b6ac2233f4c
Muhammad Arslan Arshad <109333063+Arslanlahori@users.noreply.github.com>
2023-10-03 03:09:45 +0500

    use the express validation and make my logic to store the data in db

--- BACKEND/Routes/auth.js
+++ BACKEND/Routes/auth.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const USER = require('../Models/User');
+const { body, validationResult } = require('express-validator');
+
 
 
 //create a USER using post :'api/auth'
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const users = USER(req.body);
-    users.save()
-    res.send(req.body);
+router.post('/', [
+    body('Name', 'please enter name whose length greater than 4').isLength({ min: 4 }),
+    body('Email', 'please enter valid email').isEmail(),
+    body('Password', 'please enter Password whose length greater than 5').isLength({ min: 5 })
+
+], (req, res) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+        const users = USER(req.body)
+        users.save()
+        return res.send(`Your data, ${JSON.stringify(users)}!`);
+
+    }
+    res.send({ errors: result.array() });
+    // console.log(req.body);
+    // const users = USER(req.body);
+    // users.save()
 })
 
 module.exports = router;

e34835d4fa0d07e86553154093e08f47c996e1b9
Muhammad Arslan Arshad <109333063+Arslanlahori@users.noreply.github.com>
2023-10-03 12:49:04 +0500

    using bcryptjs encrypt the password

--- BACKEND/Routes/auth.js
+++ BACKEND/Routes/auth.js
@@ -2,17 +2,22 @@ const express = require('express');
 const router = express.Router();
 const USER = require('../Models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 
 
 //create a USER using post :'api/auth'
 
-router.post('/', [
+router.post('/createuser', [
     body('Name', 'Please enter a name with a minimum length of 4 characters').isLength({ min: 4 }),
     body('Email', 'Please enter a valid email').isEmail(),
     body('Password', 'Please enter a password with a minimum length of 5 characters').isLength({ min: 5 })
 ], async (req, res) => {
+
+
+
     const result = validationResult(req);
+    //catch validation 
 
     if (!result.isEmpty()) {
         const errors = result.array().map(error => error.msg);
@@ -20,17 +25,22 @@ router.post('/', [
     }
 
     const { Name, Email, Password } = req.body;
-
     try {
+        //chech if user is already exist
         const existingUser = await USER.findOne({ Email });
 
         if (existingUser) {
             return res.status(400).json({ error: "Email already exists" });
         }
-
-        const newUser = new USER({ Name, Email, Password });
+        //use bcrypt library to convert the password into hash
+        const salt = await bcrypt.genSalt(10);
+        const secpassword = await bcrypt.hash(req.body.Password, salt)
+        //for new user
+        const newUser = new USER({ Name, Email, Password: secpassword });
         await newUser.save();
         return res.json(newUser);
+
+        //catch error
     } catch (err) {
         console.error(err);
         return res.status(500).json({ error: "Server error" });

4f5bc49f345eb8aa775f2fbf7e6e28bc477f998d
Muhammad Arslan Arshad <109333063+Arslanlahori@users.noreply.github.com>
2023-10-06 04:08:59 +0500

    using useLocation Hook for show current path

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,13 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 
 export default function Navbar() {
+    let location = useLocation();
+    useEffect(() => {
+        //For check uselocation karta kiya hy
+        //console.log(location.pathname)
+    }, [location]);
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
@@ -13,10 +18,10 @@ export default function Navbar() {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/About">About</Link>
+                            <Link className={`nav-link ${location.pathname === "/About" ? "active" : ""}`} to="/About">About</Link>
                         </li>
 
                     </ul>
@@ -26,6 +31,6 @@ export default function Navbar() {
                     </form>
                 </div>
             </div>
-        </nav>
+        </nav >
     )
 }
